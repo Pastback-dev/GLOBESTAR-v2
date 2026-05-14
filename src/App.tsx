f09b0e1e0b6c66498b849f980d5f9b70ca3e618f
[dyad] <aymansamih383@gmail.com>
2026-05-14 20:39:46 +0100

    Reverted all changes back to version 25e400dc0df61a2ed32183caf6a478645e852ab2

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,6 @@ import CompanyRegistrationForm from "./pages/CompanyRegistrationForm.tsx";
 import BusinessImmigration from "./pages/BusinessImmigration.tsx";
 import Contact from "./pages/Contact.tsx";
 import PaymentSuccess from "./pages/PaymentSuccess.tsx";
-import Maintenance from "./pages/Maintenance.tsx";
 import ScrollToTop from "./components/ScrollToTop.tsx";
 import { LanguageProvider } from "./contexts/LanguageContext.tsx";
 
@@ -35,9 +34,6 @@ const App = () => (
           <Routes>
             {/* Home */}
             <Route path="/" element={<Index />} />
-
-            {/* Maintenance */}
-            <Route path="/maintenance" element={<Maintenance />} />
 
             {/* About */}
             <Route path="/our-team" element={<OurTeam />} />
